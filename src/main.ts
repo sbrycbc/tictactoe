70e3454906ad4c12a13918190de602f91da11e5b
sbrycbc <sabriyecbc@gmail.com>
2024-11-05 16:18:38 +0100

    added module.ts und more....

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
+import {AppModule } from './app/app.module';
 
-bootstrapApplication(AppComponent, appConfig)
+bootstrapApplication( AppModule)
   .catch((err) => console.error(err));
